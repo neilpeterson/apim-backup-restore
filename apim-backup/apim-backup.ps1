9546dd6862b14e68f823d167be19872929e98d57
Neil Peterson <nepeters@microsoft.com>
2023-09-17 10:42:13 -0700

    updates

--- apim-backup/apim-backup.ps1
+++ apim-backup/apim-backup.ps1
@@ -1,1 +1,15 @@
 Connect-AzAccount -Identity
+
+$apiManagementName = Get-AutomationVariable -Name "apiManagementName"
+$apiManagementResourceGroup = Get-AutomationVariable -Name "apiManagementResourceGroup"
+$storageAccountName = Get-AutomationVariable -Name "storageAccountName"
+# $storageResourceGroup = Get-AutomationVariable -Name "storageResourceGroup"
+$containerName = Get-AutomationVariable -Name "containerName"
+$blobName = Get-AutomationVariable -Name "blobName"
+# $backupName = 'test-backup'
+$msiClientId = Get-AutomationVariable -Name "msiClientId"
+
+$storageContext = New-AzStorageContext -StorageAccountName $storageAccountName
+
+Backup-AzApiManagement -ResourceGroupName $apiManagementResourceGroup -Name $apiManagementName -StorageContext $storageContext `
+ -TargetContainerName $containerName -TargetBlobName $blobName -AccessType "UserAssignedManagedIdentity" -IdentityClientId $msiClientId -PassThru
