aa413ed353a1e98e808f04dc0b7d6de07914325e
Neil Peterson <nepeters@microsoft.com>
2023-09-18 18:43:13 -0700

    transfer

--- apim-backup/apim-backup.ps1
+++ apim-backup/apim-backup.ps1
@@ -3,13 +3,11 @@ Connect-AzAccount -SubscriptionId '7d87d11e-2aaa-4d69-85bf-a1c11503f96d' -Identi
 $apiManagementName = Get-AutomationVariable -Name "apiManagementName"
 $apiManagementResourceGroup = Get-AutomationVariable -Name "apiManagementResourceGroup"
 $storageAccountName = Get-AutomationVariable -Name "storageAccountName"
-# $storageResourceGroup = Get-AutomationVariable -Name "storageResourceGroup"
 $containerName = Get-AutomationVariable -Name "containerName"
 $blobName = Get-AutomationVariable -Name "blobName"
-# $backupName = 'test-backup'
-$msiClientId = Get-AutomationVariable -Name "msiClientId"
 
 $storageContext = New-AzStorageContext -StorageAccountName $storageAccountName
 
-Backup-AzApiManagement -ResourceGroupName $apiManagementResourceGroup -Name $apiManagementName -StorageContext $storageContext `
- -TargetContainerName $containerName -TargetBlobName $blobName -AccessType "UserAssignedManagedIdentity" -IdentityClientId $msiClientId -PassThru
+Backup-AzApiManagement -ResourceGroupName $apiManagementResourceGroup -Name $apiManagementName `
+    -StorageContext $storageContext -TargetContainerName $containerName -TargetBlobName $backupName `
+    -AccessType SystemAssignedManagedIdentity
